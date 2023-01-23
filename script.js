008d51c3178ddbbc60882f243f168a42a2f9c43b
Shareef <Shareef4815@gmail.com>
2023-01-23 19:23:04 +0300

    Make getComputerChoice function

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 let randomNumber = Math.floor(Math.random() * 100) + 1;
 
-console.log(randomNumber);
+
+function getComputerChoice () {
+    if (randomNumber <= 33){
+        return "Rock";
+    } else if (randomNumber > 33 && randomNumber < 66){
+        return "Paper";
+    } else if (randomNumber >= 66) {
+        return "Scissors";
+    }
+
+}
+console.log(getComputerChoice());
