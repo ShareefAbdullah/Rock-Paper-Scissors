072162d3f6a272a559f85c6c4523068b49f11c2b
Shareef <Shareef4815@gmail.com>
2023-01-25 19:57:11 +0300

    Write playRound function

--- script.js
+++ script.js
@@ -13,4 +13,26 @@ function getComputerChoice () {
 }
 
 let userChoice = prompt().toUpperCase();
-console.log(userChoice);
+let playerSelection = userChoice;
+let computerSelection = getComputerChoice()
+
+function playRound (playerSelection, computerSelection){
+    if (playerSelection === "ROCK" && computerSelection === "Paper"){
+        return "You lose! Paper beats Rock";
+    } else if (playerSelection === "ROCK" && computerSelection === "Scissors"){
+        return "You win! Rock beats Scissors";
+    } else if (playerSelection === "PAPER" && computerSelection === "Scissors"){
+        return"You lose! Scissors beat Paper";
+    } else if (playerSelection === "PAPER" && computerSelection === "Rock"){
+        return"You win! Paper beats Rock";
+    } else if (playerSelection === "SCISSORS" && computerSelection === "Rock"){
+        return "You lose! Rock beats Scissors";
+    } else if (playerSelection ==="SCISSORS" && computerSelection === "Paper"){
+        return "You win! Scissors beat Paper";
+    } else{
+        return "It's a draw! One more round";
+    }
+}
+
+console.log(playRound(playerSelection, computerSelection));
+
