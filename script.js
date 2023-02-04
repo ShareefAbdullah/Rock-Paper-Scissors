42f6aabf39423400648ffffd1fdfcdfe21b4dc8b
Shareef <Shareef4815@gmail.com>
2023-02-04 11:47:38 +0300

    Add two variables to track the game score

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice () {
 
     let randomNumber = Math.floor(Math.random() * 100) + 1;
@@ -18,16 +21,22 @@ function playRound (){
     let computerSelection = getComputerChoice();
 
     if (playerSelection === "ROCK" && computerSelection === "Paper"){
+        computerScore += 1;
         return "You lose! Paper beats Rock";
     } else if (playerSelection === "ROCK" && computerSelection === "Scissors"){
+        playerScore += 1;
         return "You win! Rock beats Scissors";
     } else if (playerSelection === "PAPER" && computerSelection === "Scissors"){
+        computerScore += 1;
         return "You lose! Scissors beat Paper";
     } else if (playerSelection === "PAPER" && computerSelection === "Rock"){
+        playerScore += 1;
         return "You win! Paper beats Rock";
     } else if (playerSelection === "SCISSORS" && computerSelection === "Rock"){
+        computerScore += 1;
         return "You lose! Rock beats Scissors";
     } else if (playerSelection ==="SCISSORS" && computerSelection === "Paper"){
+        playerScore += 1;
         return "You win! Scissors beat Paper";
     } else{
         return "It's a draw! One more round";
@@ -39,6 +48,7 @@ function game() {
     for(let i = 0; i < 5; i ++){
         console.log(playRound());
     }
+    console.log("\nThe game is over: ", "Player - " + playerScore + ";", "Computer - " + computerScore);
 }
 
 game();
