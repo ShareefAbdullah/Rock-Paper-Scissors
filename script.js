ff2d76937e519119843af6b22ce53e1e97f5da4a
ShareefAbdullah <Shareef4815@gmail.com>
2025-02-24 16:24:37 +0300

    Improve the code with DOM methods

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 let playerScore = 0;
 let computerScore = 0;
+const round = document.querySelector("#round");
+const result = document.querySelector("#result");
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors")
+let playerSelection;
+
+  rock.addEventListener("click", () => {
+  playerSelection = "ROCK";
+  game();
+})
+  paper.addEventListener("click", () => {
+  playerSelection = "PAPER";
+  game();
+})
+  scissors.addEventListener("click", () => {
+  playerSelection = "SCISSORS";
+  game();
+})
 
 function getComputerChoice () {
-
-    let randomNumber = Math.floor(Math.random() * 100) + 1;
-
+  let randomNumber = Math.floor(Math.random() * 100) + 1;
     if (randomNumber <= 33){
         return "Rock";
     } else if (randomNumber > 33 && randomNumber <= 66){
@@ -12,43 +29,46 @@ function getComputerChoice () {
     } else if (randomNumber > 66 ) {
         return "Scissors";
     }
-
 }
 
 function playRound (){
 
-    let playerSelection = prompt().toUpperCase();
+    
     let computerSelection = getComputerChoice();
 
     if (playerSelection === "ROCK" && computerSelection === "Paper"){
         computerScore += 1;
-        return "You lose! Paper beats Rock";
+        round.innerText = "You lose! Paper beats Rock";
     } else if (playerSelection === "ROCK" && computerSelection === "Scissors"){
         playerScore += 1;
-        return "You win! Rock beats Scissors";
+        round.innerText = "You win! Rock beats Scissors";
     } else if (playerSelection === "PAPER" && computerSelection === "Scissors"){
         computerScore += 1;
-        return "You lose! Scissors beat Paper";
+        round.innerText = "You lose! Scissors beat Paper";
     } else if (playerSelection === "PAPER" && computerSelection === "Rock"){
         playerScore += 1;
-        return "You win! Paper beats Rock";
+        round.innerText = "You win! Paper beats Rock";
     } else if (playerSelection === "SCISSORS" && computerSelection === "Rock"){
         computerScore += 1;
-        return "You lose! Rock beats Scissors";
+        round.innerText = "You lose! Rock beats Scissors";
     } else if (playerSelection ==="SCISSORS" && computerSelection === "Paper"){
         playerScore += 1;
-        return "You win! Scissors beat Paper";
+        round.innerText = "You win! Scissors beat Paper";
     } else{
-        return "It's a draw! One more round";
+        round.innerText = "It's a draw! One more round";
     }
 }
 
 function game() {
-
-    for(let i = 0; i < 5; i ++){
-        console.log(playRound());
-    }
-    console.log("\nThe game is over: ", "Player - " + playerScore + ";", "Computer - " + computerScore);
+    if(playerScore >= 5){
+    round.innerText = "The game is over! You won!";
+    result.innerText = `Score: Player - ${playerScore} ; Computer - ${computerScore};`;
+} else if(computerScore >= 5){
+    round.innerText = "The game is over! You lost!";
+    result.innerText = `Score: Player - ${playerScore} ; Computer - ${computerScore};`;
+} else{
+    
+    console.log(playRound());
+    result.innerText = `Score: Player - ${playerScore} ; Computer - ${computerScore};`;
+  }
 }
-
-game();
